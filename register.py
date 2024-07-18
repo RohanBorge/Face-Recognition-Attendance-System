b3cdeca0699ab4f27adb637863fef771e9e41dc8
RohanBorge <146016746+RohanBorge@users.noreply.github.com>
2024-07-19 02:49:06 +0530

    Update register.py

--- register.py
+++ register.py
@@ -21,20 +21,17 @@ class Register:
         self.var_securityA=StringVar()
         self.var_pass=StringVar()
         self.var_confpass=StringVar()
-
-db_host = os.getenv('DB_HOST', 'localhost')
-db_user = os.getenv('DB_USER', 'root')
-db_password = os.getenv('DB_PASSWORD', 'Rohan@123')
-db_name = os.getenv('DB_NAME', 'face_recognization')
-
-# Establishing the connection using environment variables
-conn = mysql.connector.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    database=db_name
-)
-cursor = conn.cursor()
+        db_host = os.getenv('DB_HOST', 'localhost')
+        db_user = os.getenv('DB_USER', 'root')
+        db_password = os.getenv('DB_PASSWORD', 'Rohan@123')
+        db_name = os.getenv('DB_NAME', 'face_recognization')
+        conn = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        cursor = conn.cursor()
 
 
         #======================bg image=============
